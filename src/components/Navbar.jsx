7affbe20038199e111d7b5400f13530dbe65d3ab
Saqib <saqib.rajput4747@gmail.com>
2024-10-30 16:55:11 +0500

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
+
+import {Menu, X} from "lucide-react";
+import { useState } from "react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants";
 const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setMobileDrawerOpen(!mobileDrawerOpen);
+    };
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 m-auto relative text-sm">
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-between">
                 <div className="flex items-center flex-shrink-0">
                     <img className="w-10 h-10 mr-2" src={logo} alt="logo" />
-                    <span className="text-xl tracking-tight">B_Dubai</span>
+                    <span className="text-xl tracking-tight">Rf_Tech</span>
                 </div>
                 <ul className="hidden lg:flex ml-14 space-x-12 ">
                     {navItems.map((item, index) => (
@@ -16,6 +24,21 @@ const Navbar = () => {
                         </li>
                     ))}
                 </ul>
+                <div className="hidden lg:flex justify-center items-center space-x-12">
+                    <a className="py-2 px-3 border rounded-md" href="#">
+                        Sign In
+                    </a>
+                    <a className="bg-gradient-to-r from-purple-500 to-purple-800 py-2 px-3 border rounded-md" href="#">
+                        Create an account
+                    </a>
+                </div>
+                <div className="lg:hidden md:flex flex-col justify-end">
+                    <button onClick={toggleNavbar}>
+                    
+
+                    </button>
+
+                </div>
             </div>
         </div>
     </nav>
