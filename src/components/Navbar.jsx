3483389528e2f0ae44adad0ee5c3424981a3492a
Saqib <saqib.rajput4747@gmail.com>
2024-10-30 16:19:36 +0500

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,11 @@ const Navbar = () => {
                     <span className="text-xl tracking-tight">B_Dubai</span>
                 </div>
                 <ul className="hidden lg:flex ml-14 space-x-12 ">
-
-
+                    {navItems.map((item, index) => (
+                        <li key={index}>
+                            <a href={item.href}>{item.label}</a>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
