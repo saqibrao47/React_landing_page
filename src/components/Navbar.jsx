c6193e02c8b3b95b4f351b75f76e1c63198d1ec1
Saqib <saqib.rajput4747@gmail.com>
2024-10-30 17:22:09 +0500

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,72 @@
-
-import {Menu, X} from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants";
 const Navbar = () => {
-    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
 
-    const toggleNavbar = () => {
-        setMobileDrawerOpen(!mobileDrawerOpen);
-    };
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
-        <div className="container px-4 m-auto relative text-sm">
-            <div className="flex items-center justify-between">
-                <div className="flex items-center flex-shrink-0">
-                    <img className="w-10 h-10 mr-2" src={logo} alt="logo" />
-                    <span className="text-xl tracking-tight">Rf_Tech</span>
-                </div>
-                <ul className="hidden lg:flex ml-14 space-x-12 ">
-                    {navItems.map((item, index) => (
-                        <li key={index}>
+      <div className="container px-4 m-auto relative text-sm">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center flex-shrink-0">
+            <img className="w-10 h-10 mr-2" src={logo} alt="logo" />
+            <span className="text-xl tracking-tight">Rf_Tech</span>
+          </div>
+          <ul className="hidden lg:flex ml-14 space-x-12 ">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="hidden lg:flex justify-center items-center space-x-12">
+            <a className="py-2 px-3 border rounded-md" href="#">
+              Sign In
+            </a>
+            <a
+              className="bg-gradient-to-r from-purple-500 to-purple-800 py-2 px-3 border rounded-md"
+              href="#"
+            >
+              Create an account
+            </a>
+          </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
+        {mobileDrawerOpen && (
+            <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                <ul>
+                    {navItems.map((item, index) =>(
+                        <li key={index} className="py-4">
                             <a href={item.href}>{item.label}</a>
                         </li>
                     ))}
                 </ul>
-                <div className="hidden lg:flex justify-center items-center space-x-12">
-                    <a className="py-2 px-3 border rounded-md" href="#">
-                        Sign In
-                    </a>
-                    <a className="bg-gradient-to-r from-purple-500 to-purple-800 py-2 px-3 border rounded-md" href="#">
-                        Create an account
-                    </a>
+                <div className="flex space-x-6">
+                <a className="py-2 px-3 border rounded-md" href="#">
+              Sign In
+            </a>
+            <a
+              className="bg-gradient-to-r from-purple-500 to-purple-800 py-2 px-3 border rounded-md"
+              href="#"
+            >
+              Create an account
+            </a>
                 </div>
-                <div className="lg:hidden md:flex flex-col justify-end">
-                    <button onClick={toggleNavbar}>
-                    
 
-                    </button>
-
-                </div>
             </div>
-        </div>
+        )
+        }
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
