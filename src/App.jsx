9647ed196970d5d5333dd0a3140a07088d8d8592
Saqib <saqib.rajput4747@gmail.com>
2024-11-01 14:11:06 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from './components/Features'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 
@@ -7,13 +8,14 @@ const App = () => {
     <Navbar />
     <div className="max-w-7xl mx-auto pt-20 px-6">
     <HeroSection />
+    <Features />
     </div>
   
    
    
    </>
    
-  )
+  );
 }
 
 export default App
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
+    <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
+        <div className="text-center">
+            <span className="bg-neutral-900 text-purple-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
+                feature
+            </span>
+        
+        </div>
+
+    </div>
+    
+  );
 }
 
 export default Features
