607fc6cb09e4d71dd829d2b69b635b7b51ec74d7
Saqib <saqib.rajput4747@gmail.com>
2024-11-02 16:06:31 +0500

    changes

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,5 +1,6 @@
 import { CheckCircle2 } from "lucide-react";
 import codeimg from "../assets/code.jpg";
+import { checklistItems } from "../constants";
 const Workflow = () => {
   return (
     <div className="mt-20">
@@ -12,6 +13,20 @@ const Workflow = () => {
       <div className="flex flex-wrap justify-center">
         <div className="mt-2 w-full lg:w-1/2">
         <img src={codeimg} alt="code" />
+        </div>
+        <div className="pt-12 w-full lg:w-1/2">
+        {checklistItems.map((item, index) =>(
+            <div key={index} className="flex mb-12">
+                <div className="text-blue-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
+                <CheckCircle2 />
+                </div>
+                <div>
+                    <h5 className="mt-1 mb-2 text-xl">{item.title}</h5>
+                    <p className="text-md text-neutral-500">{item.description}</p>
+                </div>
+            </div>
+        ))}
+
         </div>
       </div>
     </div>
